79763b737cee0294fcbf34b76774fc0e4b9bbbba
hyowon <at96@ajou.ac.kr>
2022-04-13 12:01:19 +0900

    데이터 자리에 넣기

--- BrainteaserWeb/BrainteaserWeb/settings.py
+++ BrainteaserWeb/BrainteaserWeb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'BrainteaserWeb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
