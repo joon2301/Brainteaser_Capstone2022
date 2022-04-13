01a1158baeb992aa76c83e8d9e8946c7d16f8ad4
joon2301 <joon2301@naver.com>
2022-04-13 20:56:18 +0900

    게시판 완벽한거같은데요..?

--- BrainteaserWeb/theme/views.py
+++ BrainteaserWeb/theme/views.py
@@ -1,27 +1,29 @@
-import requests
-from django.core import serializers
-from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import Board,BoardContents
-
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def list(request):
+def list(request,idx = 0):
     # IT 게시판
+    if idx != 0 :
+        idx = idx
     boards = Board.objects.filter(Category='Category1')
     boardList = []
     for i in boards:
         boardList.append(str(i).split(','))
-    print(boardList)
 
-    # 다른 게시판2
-    # boards = Board.objects.filter(Category='Category2')
-    # 다른 게시판3
-    # boards = Board.objects.filter(Category='Category3')
+    print(len(boards))
+    return render(request, 'list.html',{"boards":boardList,'index':idx})
 
-    return render(request, 'list.html',{"boards":boardList})
+def list2(request):
+    boards = Board.objects
+    boardList = Board.objects.filter(Category='Category1')
+    paginator = Paginator(boardList,'5')
+    page = request.GET.get('page',1)
+    posts = paginator.page(page)
+    return render(request, 'list.html',{"boards":boards,"posts":posts})
 
 def write(request):
     return render(request, 'write.html')
@@ -37,3 +39,10 @@ def logout(request):
     print(request.session.get('username'), "로그아웃")
     request.session.flush()
     return redirect('/')
+
+def next(request):
+
+    return list(request,1)
+
+
+
