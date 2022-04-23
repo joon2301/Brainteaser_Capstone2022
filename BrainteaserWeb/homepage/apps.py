566ce07ae08432fdf8bebf1e6f5eb1363178246b
joon2301 <joon2301@naver.com>
2022-04-24 00:50:51 +0900

    검색기능 추가

--- BrainteaserWeb/homepage/apps.py
+++ BrainteaserWeb/homepage/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from sentence_transformers import SentenceTransformer
 
 
 class HomepageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    # 60% https://huggingface.co/jhgan/ko-sbert-nli
+    embedder = SentenceTransformer("jhgan/ko-sbert-nli")
+
+    # 50% https://huggingface.co/jhgan/ko-sbert-sts
+    # embedder = SentenceTransformer("jhgan/ko-sbert-sts")
     name = 'homepage'
+
