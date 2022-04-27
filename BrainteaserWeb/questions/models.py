274a0feec97e96ea28987e2ef3f7dd96188da864
hyowonshin <at96@ajou.ac.kr>
2022-04-28 07:14:28 +0900

    데이터추가하기

--- BrainteaserWeb/questions/models.py
+++ BrainteaserWeb/questions/models.py
@@ -5,6 +5,7 @@ from datetime import date
 class Board(models.Model):
     TeaserID = models.IntegerField(verbose_name="번호", primary_key=True)
     Title = models.CharField(max_length=50, verbose_name="제목")
+    Teaser = models.TextField(verbose_name="본문")
     Category = models.CharField(max_length=15)
     AccID = models.CharField(max_length=15, verbose_name="작성자")
     Date = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
